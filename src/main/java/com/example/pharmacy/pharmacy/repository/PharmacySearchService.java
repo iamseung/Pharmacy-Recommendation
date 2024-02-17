5320128a17a19a224daea8ee15ffe1bdf112e0e9
iamseung <seesungseok@naver.com>
2024-02-17 20:33:22 +0900

    거리 계산 알고리즘 구현
    - 거리 계산을 위한 엔티티 설계, Direction
    - 서비스 로직에 필요한 PharmacyDto 구현, Entity to Dto 의 로직도 추가
    - DirectionService, Haversine formula 알고리즘을 통해 거리를 구하는 메서드 구현
    - 이를 검증하기 위한 테스트 코드 구현

---
+++ src/main/java/com/example/pharmacy/pharmacy/repository/PharmacySearchService.java
@@ -0,0 +1,31 @@
+package com.example.pharmacy.pharmacy.repository;
+
+import com.example.pharmacy.pharmacy.dto.PharmacyDto;
+import com.example.pharmacy.pharmacy.service.PharmacyRepositoryService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/*
+약국을 조회할 때 DB 뿐만 아니라 redis 에서도 사용하기 위해
+의존성을 분리하여 유연한 서비스 제공하고자 서비스 로직 추가
+ */
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacySearchService {
+    private final PharmacyRepositoryService pharmacyRepositoryService;
+
+    public List<PharmacyDto> searchPharmacyDtoList() {
+        // redis
+
+        // db
+        return pharmacyRepositoryService.findAll().stream()
+                .map(PharmacyDto::from)
+                .collect(Collectors.toList());
+    }
+}
