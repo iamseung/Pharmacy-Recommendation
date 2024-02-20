375aa250fcdd8ef5d2190718955d90959e99ff62
iamseung <seesungseok@naver.com>
2024-02-20 23:58:58 +0900

    서비스 코드 Dto 적용
    - 서비스 코드에 dto 적용, 컨트롤러 연동 목적

--- src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
@@ -3,6 +3,7 @@ package com.example.pharmacy.pharmacy.service;
 import com.example.pharmacy.api.dto.DocumentDto;
 import com.example.pharmacy.api.dto.KakaoApiResponseDto;
 import com.example.pharmacy.api.service.KakaoAddressSearchService;
+import com.example.pharmacy.direction.dto.OutputDto;
 import com.example.pharmacy.direction.entity.Direction;
 import com.example.pharmacy.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
@@ -10,8 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -21,20 +24,23 @@ public class PharmacyRecommendationService {
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
 
-    public void recommendPharmacyList(String address) {
+    public List<OutputDto> recommendPharmacyList(String address) {
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
         if(Objects.isNull(kakaoApiResponseDto) ||
                 CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input Address : {}", address);
-            return;
+            return Collections.emptyList();
         }
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
         List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
-        directionService.saveAll(directionList);
+        return directionService.saveAll(directionList)
+                .stream()
+                .map(OutputDto::from)
+                .collect(Collectors.toList());
     }
 
 }
