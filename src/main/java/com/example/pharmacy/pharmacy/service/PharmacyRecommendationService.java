c1bf523b39fcc624f563bd8c55aafe7697a17c7e
iamseung <seesungseok@naver.com>
2024-02-22 23:37:54 +0900

    base62 서비스 코드 접목
    - 구현 개요
    1. encode 한 데이터를 DB 에 저장
    2. Controller 단에서 decode 하여 redirect

--- src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
+++ src/main/java/com/example/pharmacy/pharmacy/service/PharmacyRecommendationService.java
@@ -5,11 +5,16 @@ import com.example.pharmacy.api.dto.KakaoApiResponseDto;
 import com.example.pharmacy.api.service.KakaoAddressSearchService;
 import com.example.pharmacy.direction.dto.OutputDto;
 import com.example.pharmacy.direction.entity.Direction;
+import com.example.pharmacy.direction.service.Base62Service;
 import com.example.pharmacy.direction.service.DirectionService;
+import com.example.pharmacy.pharmacy.dto.PharmacyDto;
+import com.example.pharmacy.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collections;
 import java.util.List;
@@ -23,10 +28,19 @@ public class PharmacyRecommendationService {
 
     private final KakaoAddressSearchService kakaoAddressSearchService;
     private final DirectionService directionService;
+    private final Base62Service base62Service;
+
+    // 로드뷰 URL
+    private static final String ROAD_VIEW_BASE_URL = "https://map.kakao.com/link/roadview/";
+
+    @Value("${pharmacy.recommendation.base.url}")
+    private String baseUrl;
 
     public List<OutputDto> recommendPharmacyList(String address) {
+        // 위치 기반 데이터로 변경 with 주소 검색 API
         KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService.requestAddressSearch(address);
 
+        // Validation
         if(Objects.isNull(kakaoApiResponseDto) ||
                 CollectionUtils.isEmpty(kakaoApiResponseDto.getDocumentList())) {
             log.error("[PharmacyRecommendationService recommendPharmacyList fail] Input Address : {}", address);
@@ -35,12 +49,30 @@ public class PharmacyRecommendationService {
 
         DocumentDto documentDto = kakaoApiResponseDto.getDocumentList().get(0);
 
+        // 공공기관 약국 데이터 및 거리계산 알고리즘 사용
         List<Direction> directionList = directionService.buildDirectionList(documentDto);
 
         return directionService.saveAll(directionList)
                 .stream()
-                .map(OutputDto::from)
+                .map(this::from)
                 .collect(Collectors.toList());
+    }
+
+    // Entity to Dto
+    private OutputDto from(Direction direction) {
+        /*
+            조회 시, DB Sequence 값 암호화했해서 view 에 표기
+            DirectionController에서 암호화된 값을 Decode 해서 DB 조회
+         */
+        // direction Url 은 shortenUrl 로 제공
+
+        return OutputDto.builder()
+                .pharmacyName(direction.getTargetPharmacyName())
+                .pharmacyAddress(direction.getTargetAddress())
+                .directionUrl(baseUrl + base62Service.encodingDirectionId(direction.getId()))
+                .roadViewUrl(ROAD_VIEW_BASE_URL + direction.getTargetLatitude() + "," + direction.getTargetLongitude())
+                .distance(String.format("%.2f km", direction.getDistance()))
+                .build();
     }
 
 }
