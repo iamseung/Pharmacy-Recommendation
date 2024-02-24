0df935f09b55407959b76c3dd185518e8fdba7f3
iamseung <seesungseok@naver.com>
2024-02-24 23:17:30 +0900

    Redis 도입
    - Redis Service 로직 구현
    + 테스트 코드로 CRUD 검증
    - DB 조회 전에 Redis 조회 추가
    - Redis 데이터 초기 셋팅 추가
    + Redis 세이브 기능 테스트 완료

---
+++ src/main/java/com/example/pharmacy/pharmacy/cache/PharmacyRedisTemplateService.java
@@ -0,0 +1,84 @@
+package com.example.pharmacy.pharmacy.cache;
+
+import com.example.pharmacy.pharmacy.dto.PharmacyDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRedisTemplateService {
+
+     private static final String CACHE_KEY = "PHARMACY";
+
+     private final RedisTemplate<String, Object> redisTemplate;
+     private final ObjectMapper objectMapper;
+
+     // HashKey(CACHE_KEY), SubKey(PK), Dto with JSON
+     private HashOperations<String, String, String> hashOperations;
+
+     // 생성자 주입 이후
+     @PostConstruct
+     public void init() {
+          this.hashOperations = redisTemplate.opsForHash();
+     }
+
+     public void save(PharmacyDto pharmacyDto) {
+          if(Objects.isNull(pharmacyDto) || Objects.isNull(pharmacyDto.getId())) {
+               log.error("Required Values must not be null");
+               return;
+          }
+
+          try {
+               hashOperations.put(
+                       CACHE_KEY,
+                       pharmacyDto.getId().toString(),
+                       serializePharmacyDto(pharmacyDto)
+               );
+               log.info("[PharmacyRedisTemplateService save success] id : {}", pharmacyDto.getId());
+          } catch (Exception e) {
+               log.error("[PharmacyRedisTemplateService save error] {}", e.getMessage());
+          }
+     }
+
+     public List<PharmacyDto> findAll() {
+          try {
+               List<PharmacyDto> list = new ArrayList<>();
+               for (String value : hashOperations.entries(CACHE_KEY).values()) {
+                    PharmacyDto pharmacyDto = deserializePharmacyDto(value);
+                    list.add(pharmacyDto);
+               }
+               return list;
+
+          } catch (Exception e) {
+               log.error("[PharmacyRedisTemplateService findAll error]: {}", e.getMessage());
+               return Collections.emptyList();
+          }
+     }
+
+     public void delete(Long id) {
+          hashOperations.delete(CACHE_KEY, String.valueOf(id));
+          log.info("[PharmacyRedisTemplateService delete]: {} ", id);
+     }
+
+     // String to Dto
+     private String serializePharmacyDto(PharmacyDto pharmacyDto) throws JsonProcessingException {
+          return objectMapper.writeValueAsString(pharmacyDto);
+     }
+
+     // Dto to String
+     private PharmacyDto deserializePharmacyDto(String value) throws JsonProcessingException {
+          return objectMapper.readValue(value, PharmacyDto.class);
+     }
+}
